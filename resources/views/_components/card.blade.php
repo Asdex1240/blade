e32388cf1be454daa0a0e2c6986f8a72d4909225
Juan Jose Moreno Barron <jose.morenobrn@uanl.edu.mx>
2024-05-15 13:14:24 -0600

    Comenzamos aprendiendo blade

---
+++ resources/views/_components/card.blade.php
@@ -0,0 +1,5 @@
+<div style="border: 1px solid black; ">
+    <h3>{{ $title }}</h3>
+    <p>{{ $content}}</p>
+    <img src="{{asset('assets/img/imagenprueba.png')}}" style="width: 128px">
+</div>
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.landing')
 
-
 @section('title', 'Servies')
 @section('content')
-<h1>Services </h1>
+    <h1>Services</h1>
+    @component('_components.card')
+        @slot('title', 'Service 1')
+        @slot('content', 'Lorem ipsum dolor ser almet')
+    @endcomponent
 @endsection
